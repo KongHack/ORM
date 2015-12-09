5f6b5a51680bdfeb1c37eda5b33420ef0096b055
Mitch <mitch.dodge@mhc.org>
2015-12-09 08:09:21 -0500

    Add a backtrace to the ORMException class

--- src/ORMException.php
+++ src/ORMException.php
@@ -5,8 +5,11 @@ use \Exception;
 
 class ORMException extends Exception
 {
+    public $backtrace = null;
+
     public function __construct($message, $code = 0, Exception $previous = null)
     {
+        $this->backtrace = debug_backtrace();
         parent::__construct($message, $code, $previous);
     }
 
