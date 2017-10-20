0078a43d81fa22bc3088fdf00e5c2f98b831b8e0
Mitch <mitch.dodge@mhc.org>
2017-10-20 15:12:33 -0400

    Quick fix to master.sql

--- datamodel/audit/master.sql
+++ datamodel/audit/master.sql
@@ -4,7 +4,7 @@ CREATE TABLE __REPLACE__ (
   `audit_table` VARCHAR(64) NOT NULL ,
   `audit_version` INT(11) NOT NULL ,
   `audit_datetime_created` DATETIME NOT NULL ,
-  `audit_datetime_updated` INT NOT NULL ,
+  `audit_datetime_updated` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`audit_id`)
 ) ENGINE = InnoDB;
 
