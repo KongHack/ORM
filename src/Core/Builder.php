9e4cf4d27788feaaeee6ee147103a7336e847a04
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2020-09-15 23:46:17 -0400

    [master]: Disable audit ALTER

--- src/Core/Builder.php
+++ src/Core/Builder.php
@@ -15,12 +15,12 @@ class Builder
     protected $common = null;
 
     /**
-     * @var Database
+     * @var Database|\GCWorld\Interfaces\Database|\PDO
      */
-    protected $_db    = null;
+    protected $_db = null;
 
     /**
-     * @var Database
+     * @var Database|\GCWorld\Interfaces\Database|\PDO
      */
     protected $_audit = null;
 
@@ -41,25 +41,28 @@ class Builder
         $this->config   = $common->getConfig('audit');
         $this->common   = $common;
         $this->_db      = $common->getDatabase();
-        $this->_audit   = $common->getDatabase($this->config['connection']??'');
+        $this->_audit   = $common->getDatabase($this->config['connection'] ?? '');
         $this->database = $this->config['database'];
     }
 
     /**
      * @param string|null $schema
+     * @throws \Exception
+     * @throws \PDOException
+     * @return void
      */
     public function run(string $schema = null)
     {
         $master = $this->config['prefix'].'_GCAuditMaster';
-        if($this->database != null) {
+        if ($this->database != null) {
             $master = $this->database.'.'.$master;
         }
 
-        if(!$this->_audit->tableExists($master)) {
+        if (!$this->_audit->tableExists($master)) {
             // This will create the audit master
             $file    = $this->getDataModelDirectory().'master.sql';
             $content = file_get_contents($file);
-            $content = str_replace('__REPLACE__',$master,$content);
+            $content = str_replace('__REPLACE__', $master, $content);
             $this->_audit->exec($content);
         } else {
             $sql   = 'SHOW COLUMNS FROM '.$master;
@@ -68,30 +71,30 @@ class Builder
             $rows    = $query->fetchAll();
             $col     = 'audit_pk_set';
             $colGood = false;
-            foreach($rows as $row) {
-                if($row['Field'] == $col) {
+            foreach ($rows as $row) {
+                if ($row['Field'] == $col) {
                     $colGood = true;
                     break;
                 }
             }
-            if(!$colGood) {
+            if (!$colGood) {
                 $sql = "ALTER TABLE $master ADD `audit_pk_set` TINYINT(1) NOT NULL DEFAULT '0' AFTER `audit_table`";
                 $this->_audit->exec($sql);
             }
         }
 
         $cConfig   = new Config();
-        $ormConfig = $cConfig->getConfig()['tables']??[];
+        $ormConfig = $cConfig->getConfig()['tables'] ?? [];
 
         $existing = [];
         $sql      = "SELECT * FROM $master";
         $query    = $this->_audit->prepare($sql);
         $query->execute();
-        while($row = $query->fetch()) {
+        while ($row = $query->fetch()) {
             $existing[$row['audit_schema']][$row['audit_table']] = $row;
         }
 
-        if($schema == null) {
+        if ($schema == null) {
             $schema = $this->database ?? $this->_audit->getWorkingDatabaseName();
         }
 
@@ -104,30 +107,30 @@ class Builder
         $tables = $query->fetchAll(\PDO::FETCH_NUM);
         $preLen = \strlen($this->config['prefix']);
 
-        foreach($tables as $tRow) {
+        foreach ($tables as $tRow) {
             $table = $tRow[0];
 
             // Prevent recursion
-            if(substr($table,0,$preLen) == $this->config['prefix']) {
+            if (substr($table, 0, $preLen) == $this->config['prefix']) {
                 continue;
             }
 
             if (array_key_exists($table, $ormConfig)) {
                 $tableConfig = $ormConfig[$table];
-                if(isset($tableConfig['audit_ignore']) && $tableConfig['audit_ignore']) {
+                if (isset($tableConfig['audit_ignore']) && $tableConfig['audit_ignore']) {
                     continue;
                 }
             }
 
             $audit = $auditBase = $this->config['prefix'].$table;
-            if($this->database != null) {
+            if ($this->database != null) {
                 $audit = $this->database.'.'.$audit;
             }
 
-            if(!isset($existing[$schema][$auditBase])) {
-                if($this->_audit->tableExists($audit)) {
+            if (!isset($existing[$schema][$auditBase])) {
+                if ($this->_audit->tableExists($audit)) {
                     $existing[$schema][$auditBase]['audit_version'] = $this->_audit->getTableComment($audit);
-                    $existing[$schema][$auditBase]['audit_pk_set'] = 0;
+                    $existing[$schema][$auditBase]['audit_pk_set']  = 0;
                 } else {
                     $source = file_get_contents($this->getDataModelDirectory().'source.sql');
                     $sql    = str_replace('__REPLACE__', $audit, $source);
@@ -139,7 +142,7 @@ class Builder
             }
 
             $version = $existing[$schema][$auditBase]['audit_version'];
-            if($version < self::BUILDER_VERSION) {
+            if ($version < self::BUILDER_VERSION) {
                 $versionFiles = glob($this->getDataModelDirectory().'revisions'.DIRECTORY_SEPARATOR.'*.sql');
                 sort($versionFiles);
                 foreach ($versionFiles as $file) {
@@ -156,7 +159,7 @@ class Builder
                             $this->_audit->exec($sql);
                             $this->_audit->setTableComment($audit, $fileNumber);
                         } catch (\PDOException $e) {
-                            if(strpos($e->getMessage(),'Column already exists') !== false) {
+                            if (strpos($e->getMessage(), 'Column already exists') !== false) {
                                 continue;
                             }
 
@@ -170,7 +173,7 @@ class Builder
 
             // Ok, that updates the version.
             // Now make sure the primary key is set.
-            if(!$existing[$schema][$auditBase]['audit_pk_set']) {
+            if (!$existing[$schema][$auditBase]['audit_pk_set']) {
                 // Determine our primary field type
                 $keys  = 0;
                 $sql   = 'SHOW COLUMNS FROM '.$table;
@@ -178,15 +181,17 @@ class Builder
                 $query->execute();
                 $data = $query->fetchAll();
                 $type = null;
-                foreach($data as $datum) {
-                    if($datum['Key'] == 'PRI') {
+                foreach ($data as $datum) {
+                    if ($datum['Key'] == 'PRI') {
                         $type = $datum['Type'];
                         $keys++;
                     }
                 }
-                if($type != null && $keys == 1) {
-                    $int   = stripos($type,'int')!==false;
+                if ($type != null && $keys == 1) {
+                    // DISABLED FOR NOW
+                    /*
                     try {
+                        $int   = stripos($type,'int')!==false;
                         $sql   = 'ALTER TABLE '.$audit.' CHANGE primary_id primary_id '.$type.' DEFAULT '.
                             ($int ? '\'0\'' : '\'\'');
                         $query = $this->_audit->prepare($sql);
@@ -196,6 +201,7 @@ class Builder
                         echo 'BAD SQL',PHP_EOL,PHP_EOL,$sql,PHP_EOL,PHP_EOL;
                         throw $e;
                     }
+                    */
                 }
             }
 
@@ -210,7 +216,7 @@ class Builder
                         audit_datetime_updated = NOW()";
             $query = $this->_audit->prepare($sql);
             $query->execute([
-                ':audit_schema'  => $this->database??$schema,
+                ':audit_schema'  => $this->database ?? $schema,
                 ':audit_table'   => $auditBase,
                 ':audit_version' => self::BUILDER_VERSION,
             ]);
